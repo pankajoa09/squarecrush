a67f50df9a2a574ad88a5c3136018794c2a39761
Pankaj Ahuja <pankaj.oa.09@gmail.com>
2017-10-10 18:28:58 +0700

    fixed the columns so that they are columns not rows.

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,7 +1,9 @@
 import com.sun.imageio.plugins.gif.GIFImageReader;
 import com.sun.rowset.internal.Row;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.effect.Lighting;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -21,8 +23,9 @@ public class View {
 
     //private static GridPane columnOfBlocksPane = new GridPane();
     //private static GridPane rowOfColumnsPane = new GridPane();
-    private static StackPane stackPane = new StackPane();
-    private static GridPane mainGridPane = new GridPane();
+    private final Stage primaryStage;
+    private final StackPane stackPane = new StackPane();
+    private final GridPane mainGridPane = new GridPane();
     private final static int MAX_OBJECTS = 3;
     private final static int WIDTH = 5;
     private final static int HEIGHT = 5;
@@ -31,11 +34,16 @@ public class View {
 
     Debug debug = new Debug();
 
-    public void initializeStage(Stage primaryStage){
 
+    public View(Stage primaryStage){
+        this.primaryStage = primaryStage;
+    }
 
-        GridPane rowOfColumnsPane = refreshRowOfColumnsPane(createRowOfColumns());
+    public void initializeStage(RowOfColumns rowOfColumns){
+
+        GridPane rowOfColumnsPane = refreshRowOfColumnsPane(rowOfColumns);
         mainGridPane.add(rowOfColumnsPane,1,1);
+        stackPane.getChildren().clear();
         stackPane.getChildren().addAll(mainGridPane);
         Scene scene = new Scene(stackPane, WIDTH*BLOCK_RECTANGLE_SIZE, HEIGHT*BLOCK_RECTANGLE_SIZE);
         primaryStage.setTitle("CRUSHMEH");
@@ -45,20 +53,7 @@ public class View {
 
 
 // this is a board being created gonna move it to the Controller soon.
-    public RowOfColumns createRowOfColumns(){
-        RowOfColumns rowOfColumns = new RowOfColumns();
-        for (int i=0; i<WIDTH; i++){
-            ColumnOfBlocks columnOfBlocks = new ColumnOfBlocks();
-            for (int j=0; j<HEIGHT; j++){
-                Block block = new Block();
-                block.createRandomBlock(i,j,BLOCK_IMAGE_POOL_SIZE);
-                columnOfBlocks.addBlock(block);
-            }
-            rowOfColumns.addColumnOfBlocks(columnOfBlocks);
-        }
-        debug.printRowOfColumns(rowOfColumns);
-        return rowOfColumns;
-    }
+
 
 
 // A RowOfColumns contains ColumnOfBlocks' contains Blocks
@@ -71,7 +66,7 @@ public class View {
         int index=0;
         for (ColumnOfBlocks columnOfBlocks: rowOfColumns.getContainingColumns()){
             GridPane columnOfBlocksPane = refreshColumnOfBlocksPane(columnOfBlocks, rowOfColumns);
-            rowOfColumnsPane.setRowIndex(columnOfBlocksPane,index);
+            rowOfColumnsPane.setColumnIndex(columnOfBlocksPane,index);
             rowOfColumnsPane.getChildren().addAll(columnOfBlocksPane);
             index++;
 
@@ -84,7 +79,7 @@ public class View {
         int index = 0;
         for (Block block: columnOfBlocks.getContainingBlocks()){
             Rectangle blockRectangle = refreshBlockRectangle(block,rowOfColumns);
-            columnOfBlocksPane.setColumnIndex(blockRectangle,index);
+            columnOfBlocksPane.setRowIndex(blockRectangle,index);
             columnOfBlocksPane.getChildren().addAll(blockRectangle);
             index++;
         }
@@ -101,12 +96,26 @@ public class View {
         blockRectangle.setFill(new ImagePattern(block.getBlockImage().getImage()));
         blockRectangle.setOnMouseClicked(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent t) {
-                Controller controller = new Controller();
+                Controller controller = new Controller(primaryStage);
                 controller.rectangleBlockClickHandler(columnNumber,positionInColumn,rowOfColumns);
 
             }
         });
         return blockRectangle;
+    }
+
+    public void AnimateShit(Rectangle rectangle){
+        Group p = new Group();
+        p.setTranslateX(80);
+        p.setTranslateY(80);
+        rectangle.setEffect(new Lighting());
+        final StackPane stackPane = new StackPane();
+        stackPane.getChildren().setAll(rectangle);
+        stackPane.setLayoutX(rectangle.getLayoutX());
+        stackPane.setLayoutY(rectangle.getLayoutY());
+        p.getChildren().add(stackPane);
+
+
     }
 
 
